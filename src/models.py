b5422303466e9424978e9f03e8e37e18f95e01c4
AntonioMora <126864866+Antoniomorales17@users.noreply.github.com>
2023-06-28 08:41:21 +0000

    diagrama

--- src/models.py
+++ src/models.py
@@ -7,25 +7,55 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(90), unique=True, nullable=False)
+    relacion_posts= relationship("post", backref="user", lazy=True)
+    relacion_commnet= relationship("commnet", backref="user", lazy=True)
+    relacion_followers= relationship("followers", backref="user", lazy=True)
+    relacion_media= relationship("media", backref="user", lazy=True)
+    
 
-    def to_dict(self):
+
+
+class Post(Base):
+     __tablename__ = 'post'
+     id = Column(Integer, primary_key=True)
+     user_id = Column(Integer, ForeignKey('user.id'))
+     relacion_comment= relationship("comment", backref="user", lazy=True)
+
+class Comment (Base):
+        __tablename__ = 'comment'
+        id=Column (Integer, primary_key=True)
+        coment_text=Column(String(250))
+        author_id=Column(Integer, ForeignKey("user.id"))
+        post_id = Column(Integer, ForeignKey("post.id"))
+
+class Followers(Base):
+         __tablename__ = 'followers'
+         id=Column(Integer, primary_key=True)
+         user_from_id=Column(Integer, ForeignKey("user.id"))
+         user_to_id=Column(Integer, ForeignKey("user.id"))
+
+class Media (Base):
+        __tablename__ = 'follower'
+        id=Column(Integer, primary_key=True)
+        post_id=Column(Integer, ForeignKey("user.id"))
+
+    
+
+
+   
+   
+
+    
+
+def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
