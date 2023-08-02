ed5b3abe59f32d59f232f6d2f44fa9800aa19f26
AntonioMora <126864866+Antoniomorales17@users.noreply.github.com>
2023-08-02 15:10:32 +0000

    models

--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Table
+from sqlalchemy.orm import relationship, declarative_base, DeclarativeBase
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
@@ -9,59 +9,53 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = "user"
     id = Column(Integer, primary_key=True)
-    username = Column(String(250), nullable=False)
-    firstname = Column(String(250), nullable=False)
-    lastname = Column(String(250), nullable=False)
-    email = Column(String(90), unique=True, nullable=False)
-    relacion_posts= relationship("post", backref="user", lazy=True)
-    relacion_commnet= relationship("commnet", backref="user", lazy=True)
-    relacion_followers= relationship("followers", backref="user", lazy=True)
-    relacion_media= relationship("media", backref="user", lazy=True)
-    
+    username = Column(String(12), nullable=False, unique=True)
+    first_name = Column(String(30), nullable=False)
+    last_name = Column(String(30), nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=False)
 
+
+followers = Table(
+    "followers",
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column("user_from_id", ForeignKey("user.id")),
+    Column("user_to_id", ForeignKey("user.id")),
+)
 
 
 class Post(Base):
-     __tablename__ = 'post'
-     id = Column(Integer, primary_key=True)
-     user_id = Column(Integer, ForeignKey('user.id'))
-     relacion_comment= relationship("comment", backref="user", lazy=True)
-
-class Comment (Base):
-        __tablename__ = 'comment'
-        id=Column (Integer, primary_key=True)
-        coment_text=Column(String(250))
-        author_id=Column(Integer, ForeignKey("user.id"))
-        post_id = Column(Integer, ForeignKey("post.id"))
-
-class Followers(Base):
-         __tablename__ = 'followers'
-         id=Column(Integer, primary_key=True)
-         user_from_id=Column(Integer, ForeignKey("user.id"))
-         user_to_id=Column(Integer, ForeignKey("user.id"))
-
-class Media (Base):
-        __tablename__ = 'follower'
-        id=Column(Integer, primary_key=True)
-        post_id=Column(Integer, ForeignKey("user.id"))
-
-    
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(ForeignKey("user.id"))
 
 
-   
-   
+class Media(Base):
+    __tablename__ = "media"
+    id = Column(Integer, primary_key=True)
+    post_id = Column(ForeignKey("post.id"), nullable=False)
+    url = Column(String, nullable=False)
+    type = Column(String, nullable=False)
 
-    
 
-def to_dict(self):
-        return {}
+comments = Table(
+    "comments",
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column("comment", String, nullable=False),
+    Column("post_id", ForeignKey("post.id"), nullable=False),
+    Column("author_id", ForeignKey("user.id"), nullable=False),
+)
+
 
 ## Draw from SQLAlchemy base
 try:
-    result = render_er(Base, 'diagram.png')
+    result = render_er(Base, "diagram.png")
     print("Success! Check the diagram.png file")
 except Exception as e:
     print("There was a problem genering the diagram")
-    raise e
+    raise 
